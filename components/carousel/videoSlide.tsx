bf1f22c6c72e0b413f4c49599b840348dc4a9171
Dmitry Vasilyev <vaseker@gmail.com>
2021-03-08 15:13:53 +0500

    Polishing

--- components/carousel/videoSlide.tsx
+++ components/carousel/videoSlide.tsx
@@ -38,26 +38,25 @@ const VideoSlide = React.forwardRef<HTMLVideoElement, Props>((props, ref) => {
     }, []);
 
     const titleText = mainTitle ? RichText.asText(mainTitle) : '';
+    const videoProps = {
+        className: styles.img,
+        autoPlay: false,
+        loop: false,
+        muted: true,
+        playsInline: true,
+        controls: false,
+        ref,
+        poster: poster,
+        preload: 'metadata',
+        'data-novideo': videoSrc ? undefined : true,
+        pip: 'false'
+    } as JSX.IntrinsicElements['video'];
 
     return (
         <div className={classNames(styles.slide, 'h-screen sm:h-xv')}>
             <Link {...link}>
                 <a className={styles.inner} aria-label={titleText}>
-                    <video
-                        className={styles.img}
-                        autoPlay={false}
-                        loop={false}
-                        muted
-                        playsInline
-                        controls={false}
-                        ref={ref}
-                        poster={poster}
-                        preload="metadata"
-                        data-novideo={videoSrc ? undefined : true}
-                        pip={false}
-                    >
-                        {videoSrc && <source src={videoSrc} type={getMime(videoSrc)} />}
-                    </video>
+                    <video {...videoProps}>{videoSrc && <source src={videoSrc} type={getMime(videoSrc)} />}</video>
                     <div
                         className="absolute top-0 bottom-0 left-0 right-0 flex flex-col justify-end sm:flex-row sm:justify-center"
                         aria-hidden="true"
@@ -65,8 +64,16 @@ const VideoSlide = React.forwardRef<HTMLVideoElement, Props>((props, ref) => {
                         <i className="block h-72 sm:hidden"></i>
                         <Container className="flex flex-col sm:flex-grow overflow-hidden mb-24 sm:m-auto">
                             <div className="text-sm mb-2">{leadZero(index + 1)}</div>
-                            {foreignTitle && <div className="text-xs uppercase tracking-widest mb-4">{RichText.asText(foreignTitle)}</div>}
-                            {titleText && <h3 className="text-4-5xl font-bold sm:text-5xl sm:font-extrabold leading-tight mb-6">{titleText}</h3>}
+                            {foreignTitle && (
+                                <div className="text-xs uppercase tracking-widest mb-4">
+                                    {RichText.asText(foreignTitle)}
+                                </div>
+                            )}
+                            {titleText && (
+                                <h3 className="text-4-5xl font-bold sm:text-5xl sm:font-extrabold leading-tight mb-6">
+                                    {titleText}
+                                </h3>
+                            )}
                             {description && <p className="text-sm leading-6">{RichText.asText(description)}</p>}
                             <Arrow className="w-24 sm:w-32 text-xxs sm:text-xs opacity-60 bg-white" />
                         </Container>
