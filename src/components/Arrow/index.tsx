0f857b189ed45ad838fccb9c485e255dfe38178e
Dmitry Vasilyev <vaseker@yandex-team.ru>
2021-08-08 17:49:04 +0500

    Fix arrow style

--- src/components/Arrow/index.tsx
+++ src/components/Arrow/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { classNames } from '@lib/classNames';
 
-import styles from './arrow.module.css';
+import styles from './Arrow.module.css';
 
 const Arrow: React.FC<{ className?: string }> = ({ className }) => (
     <i className={classNames(styles.arrow, className, 'block relative h-px my-8 mx-0')} />
