eb70168a0002a5188455969ebaca90c93aa3f4cb
Dmitry Vasilyev <vaseker@yandex-team.ru>
2021-08-08 19:39:02 +0500

    Mobile header

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,7 +7,6 @@ import { classNames } from '@lib/classNames';
 const Menu: React.FC<{ whiteFlag: boolean }> = ({ whiteFlag }) => {
     const { messages } = useI18N();
     const items = useMemo(() => [
-        { name: 'main', href: '/' },
         { name: 'about', href: '/pages/about' },
         { name: 'projects', href: '/projects' },
         { name: 'career', href: '/pages/career' },
@@ -15,13 +14,13 @@ const Menu: React.FC<{ whiteFlag: boolean }> = ({ whiteFlag }) => {
 
     return (
         <nav className="flex flex-col content-end">
-            <ul className="sm:flex flex-row justify-between items-stretch min-h-full hidden">
+            <ul className="flex flex-row justify-between items-stretch min-h-full">
                 {items.map(({ name, href }) => (
                     <li className="" key={name}>
                         <Link href={href}>
                             <a
                                 className={classNames(
-                                    'flex items-center min-h-full px-4 text-sm',
+                                    'flex items-center min-h-full px-2 text-sm sm:px-4',
                                     whiteFlag ? 'text-white' : undefined
                                 )}
                             >
