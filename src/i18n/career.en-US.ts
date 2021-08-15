32e3abf8d8b4f25871c458edb05225cb1d744939
Dmitry Vasilyev <vaseker@yandex-team.ru>
2021-08-15 20:39:09 +0500

    International email

--- src/i18n/career.en-US.ts
+++ src/i18n/career.en-US.ts
@@ -6,7 +6,7 @@ export default {
             text: [
                 'Therefore, we are constantly open to new participants.',
                 'Do you have a positive experience in designing public or private interiors, small forms, architectural objects? No matter who you are-a designer, architect, designer or manager, we are always happy to see you as a job applicant in our company. ' +
-                'Send your resume or a link to your portfolio to info@transforma.ru'
+                'Send your resume or a link to your portfolio to info@transforma.design'
             ]
         }
     }
