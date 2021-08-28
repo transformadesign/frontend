01cfff06cc86cf5e41cd52c64574e00440bc18a5
Dmitry Vasilyev <vaseker@yandex-team.ru>
2021-08-28 19:28:41 +0500

    Projects removal

--- src/i18n/projects.en-US.ts
+++ src/i18n/projects.en-US.ts
@@ -5,12 +5,7 @@ import villa21 from './projects/villa-21.en-US';
 import villaSPA from './projects/villa-spa.en-US';
 import kozikhinskyApt from './projects/kozikhinsky-apt.en-US';
 import wellnessTikhvin from './projects/wellness-tikhvin.en-US';
-import terraceBenilux from './projects/terrace-benilux.en-US';
 import golfVilla from './projects/golf-villa.en-US';
-import cabana from './projects/cabana.en-US';
-import snegiriAptAndTerrace from './projects/snegiri-apt-and-terrace.en-US';
-import loungeBarBenilux from './projects/lounge-bar-benilux.en-US';
-import snegiriApt from './projects/snegiri-apt.en-US';
 
 const projects = [
     villa2,
@@ -20,12 +15,7 @@ const projects = [
     villaSPA,
     kozikhinskyApt,
     wellnessTikhvin,
-    terraceBenilux,
     golfVilla,
-    cabana,
-    snegiriAptAndTerrace,
-    loungeBarBenilux,
-    snegiriApt
 ];
 
 export default projects.reduce((result, project) => {
