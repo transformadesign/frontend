d70255257327fba76410d5359917c60ad63fe1e6
Dmitry Vasilyev <vaseker@yandex-team.ru>
2021-09-04 18:04:44 +0500

    Cut redesign

--- src/components/Cut/index.tsx
+++ src/components/Cut/index.tsx
@@ -27,13 +27,13 @@ const Cut: React.FC<Props> = ({ data, className, preset}) => {
     }, [preset]);
 
     return (
-        <section className={classNames('mb-14 pb-24 text-white', layout.fontSize, layout.sectionCn, className)}>
-            <Container className={classNames('pt-8 pb-24 mb-8', layout.containerCn)}>
-            {
-                data.text.map((text, index) => (
-                    <p key={index} className="pt-4 px-10 mr-40">{text}</p>
-                ))
-            }
+        <section className={classNames('mb-14 sm:pb-24 pb-0 text-white', layout.fontSize, layout.sectionCn, className)}>
+            <Container className={classNames('pt-8 pb-24 mb-8 box-content sm:px-12 px-0', layout.containerCn)}>
+                {
+                    data.text.map((text, index) => (
+                        <p key={index} className="pt-4 sm:mr-40 mr-0 px-3">{text}</p>
+                    ))
+                }
             </Container>
         </section>
     );
