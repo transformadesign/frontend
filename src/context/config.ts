cb17e6fd05b668c7efdebb0ddb944825dff2148b
Dmitry Vasilyev <vaseker@yandex-team.ru>
2022-07-05 13:26:42 +0500

    Change pinterest link

--- src/context/config.ts
+++ src/context/config.ts
@@ -3,7 +3,7 @@ import { createContext } from 'react';
 const ConfigCtx = createContext({
     socials: [
         { name: 'Instagram', url: 'https://www.instagram.com/transforma.design/' },
-        { name: 'Pinterest', url: 'https://www.pinterest.ru/studiodvoesrl/_saved/' },
+        { name: 'Pinterest', url: 'https://www.pinterest.com/transformadesignstudio/' },
     ]
 });
 
